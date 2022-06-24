01a6a06c8d2e9478bf57b527c73770739fd7fa07
Brandon Flores <brandonflores647@gmail.com>
2022-06-24 13:38:18 -0400

    finished bookings model/associations

---
+++ backend/db/models/booking.js
@@ -0,0 +1,36 @@
+'use strict';
+module.exports = (sequelize, DataTypes) => {
+  const Booking = sequelize.define('Booking', {
+    spotId: {
+      allowNull: false,
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'Spots'
+      },
+    },
+    userId: {
+      allowNull: false,
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'Users'
+      },
+    },
+    startDate: {
+      allowNull: false,
+      type: DataTypes.DATE
+    },
+    endDate: {
+      allowNull: false,
+      type: DataTypes.DATE
+    },
+  }, {});
+  Booking.associate = function(models) {
+    Booking.belongsTo(models.Spot, {
+      foreignKey: 'spotId'
+    });
+    Booking.belongsTo(models.User, {
+      foreignKey: 'userId'
+    });
+  };
+  return Booking;
+};
