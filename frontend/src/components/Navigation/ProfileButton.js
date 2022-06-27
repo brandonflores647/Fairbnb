080c7783b004e0db95e49e88fc56fea0a4e3e01c
Brandon Flores <brandonflores647@gmail.com>
2022-06-27 16:26:01 -0400

    finished profile button functionality

--- frontend/src/components/Navigation/ProfileButton.js
+++ frontend/src/components/Navigation/ProfileButton.js
@@ -1,9 +1,11 @@
 import { useState, useEffect } from "react";
 import { useDispatch } from 'react-redux';
+import { useHistory } from 'react-router-dom';
 import * as sessionActions from '../../store/session';
 import './Navigation.css';
 
 function ProfileButton({ user }) {
+    const history = useHistory();
     const dispatch = useDispatch();
     const [showMenu, setShowMenu] = useState(false);
 
@@ -11,6 +13,14 @@ function ProfileButton({ user }) {
         if (showMenu) return;
         setShowMenu(true);
     };
+
+    const handleDemoLogin = () => {
+        const emails = ['demo@user.io', 'user1@user.io', 'user2@user.io'];
+        return dispatch(sessionActions.login({
+            credential: emails[Math.floor(Math.random()*emails.length)],
+            password: 'P@ssw0rd!'
+        }))
+    }
 
     useEffect(() => {
         if (!showMenu) return;
@@ -31,21 +41,31 @@ function ProfileButton({ user }) {
 
     return (
         <>
-            <button onClick={openMenu} id="nav-bar-profile-button">
-                <i class="fa-solid fa-bars fa-xl" id='nav-bar-profile-menu-icon'></i>
-                <svg id='nav-bar-profile-icon' data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
-                    <path d="M10,0A10,10,0,1,0,20,10,10,10,0,0,0,10,0Zm0,18.3a8.28,8.28,0,0,1-6.49-3.14,8.12,8.12,0,0,1,4.21-2.88,4.25,4.25,0,1,1,4.56,0,8.12,8.12,0,0,1,4.21,2.88A8.28,8.28,0,0,1,10,18.3Z"/>
-                </svg>
-            </button>
-            {showMenu && (
-                <ul className="profile-dropdown">
-                    <li>{user.username}</li>
-                    <li>{user.email}</li>
-                    <li>
-                        <button onClick={logout}>Log Out</button>
-                    </li>
-                </ul>
-            )}
+            <div className='nav-bar-btn-dropdown-container'>
+                <button onClick={openMenu} id="nav-bar-profile-button">
+                    <i class="fa-solid fa-bars fa-xl" id='nav-bar-profile-menu-icon'></i>
+                    <svg id='nav-bar-profile-icon' data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
+                        <path d="M10,0A10,10,0,1,0,20,10,10,10,0,0,0,10,0Zm0,18.3a8.28,8.28,0,0,1-6.49-3.14,8.12,8.12,0,0,1,4.21-2.88,4.25,4.25,0,1,1,4.56,0,8.12,8.12,0,0,1,4.21,2.88A8.28,8.28,0,0,1,10,18.3Z"/>
+                    </svg>
+                </button>
+                {showMenu && user && (
+                    <div className="nav-bar-profile-dropdown-menu">
+                        <div id='nav-pd-user-info'>
+                            <p className='nav-pd-text' id='nav-pd-username'>{user.username}</p>
+                            <p className='nav-pd-text' >{user.email}</p>
+                        </div>
+                        <p className='nav-pd-text' id='nav-pd-profile' >TODO: ADD PROFILE BUTTON</p>
+                        <p className='nav-pd-text' id='nav-pd-logout' onClick={logout}>Log Out</p>
+                    </div>
+                )}
+                {showMenu && !user && (
+                    <div className="nav-bar-profile-dropdown-menu">
+                        <p className='nav-pd-text' id='nav-pd-signup' onClick={() => history.push('/signup')}>Sign Up</p>
+                        <p className='nav-pd-text' id='nav-pd-login' onClick={() => history.push('/login')}>Log in</p>
+                        <p className='nav-pd-text' id='nav-pd-demologin' onClick={handleDemoLogin}>Demo Login</p>
+                    </div>
+                )}
+            </div>
         </>
     );
 }
