fc901a8b6f307d48e5cec956fed13b79d938e808
Brandon Flores <brandonflores647@gmail.com>
2022-06-30 19:10:47 -0400

    generating spot cards on splash page with image slider

---
+++ frontend/src/components/SpotsContainer/SpotCard.js
@@ -0,0 +1,39 @@
+import { useState } from 'react';
+
+const SpotCard = ({ spot }) => {
+
+    const [current, setCurrent] = useState(0);
+
+    let length = Object.values(spot.images).length;
+
+    return (
+        <div className='individual-spot'>
+            <div className='img-container'>
+                {Object.values(spot.images).map((img, i) => {
+                    return (
+                        <div className={i === current ? 'slide-active' : 'slide'} key={i}>
+                            {i === current ?
+                                <img className='individual-spot-img' src={img} alt=''></img>
+                                : null}
+                        </div>
+                    )
+                })}
+                {length > 1 ?
+                    <>
+                        <button className='spot-card-leftarrow' onClick={
+                            () => setCurrent(current === 0 ? length-1 : current - 1)
+                        }><i className="fa-solid fa-chevron-left fa-sm"></i></button>
+                        <button className='spot-card-rightarrow' onClick={
+                            () => setCurrent(current === length-1 ? 0 : current + 1)
+                        }><i className="fa-solid fa-chevron-right fa-sm"></i></button>
+                    </>
+                    : null
+                }
+            </div>
+            <p>{spot.name}</p>
+            <p>${spot.price} night</p>
+        </div>
+    )
+}
+
+export default SpotCard;
--- frontend/src/components/SpotsContainer/index.js
+++ frontend/src/components/SpotsContainer/index.js
@@ -1,16 +1,24 @@
 import { useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { getAllSpots } from '../../store/spot';
+import SpotCard from './SpotCard';
+
+import './SpotsContainer.css';
 
 const SpotsContainer = () => {
+    const spots = useSelector(state => state.spot);
     const dispatch = useDispatch();
 
     useEffect(() => {
         dispatch(getAllSpots())
-    }, []);
+    }, [dispatch]);
 
     return (
-        <div>test</div>
+        <div className='spots-container'>
+            {Object.values(spots).map(spot => {
+                return <SpotCard spot={spot} />
+            })}
+        </div>
     );
 }
 
